4b02647ec50cb59f2a810df0b28f9840ecf60d2a
Banman03 <mckeownbaenan123@gmail.com>
2026-08-26 13:00:44 -0500

    added network scan.

--- app/src/main/java/java_http/App.java
+++ app/src/main/java/java_http/App.java
@@ -3,15 +3,17 @@ package java_http;
 public class App {    
     public static void main(String[] args) {
         Initialize init = new Initialize();
+        NetworkScan ns = new NetworkScan();
 
         switch(init.getRole()) {
             case Role.SERVER ->
                 System.out.println("You are a server.");
             case Role.CLIENT ->
                 System.out.println("You are a client.");
-            default ->
+            default -> {
                 System.err.println("Unknown role. Exiting");
                 return;
+            }
         }
 
 
--- app/src/main/java/java_http/NetworkScan.java
+++ app/src/main/java/java_http/NetworkScan.java
@@ -1,23 +1,58 @@
 package java_http;
 import java.net.InetAddress;
-import java.util.Scanner;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.*;
+public class NetworkScan {
+    private ArrayList<NetworkInterface> networks;
+    private ArrayList<ArrayList<InetAddress>> networkAddresses;
+    private String networkDisplayName;
 
-public class Network {
-    private InetAddress address;
-
-    public Network() {
-        System.out.println("Would you like to perform a network scan?");
+    public NetworkScan() {
+        System.out.println("Would you like to perform a network scan? (y / n)");
         Scanner sc = new Scanner(System.in);
+
+        networkAddresses = new ArrayList<>();
+
         if (sc.hasNext()) {
             String response = sc.nextLine();
             if (response.equals(Responses.YES)) {
-                
+                runScan();
+            } else if (response.equals(Responses.NO)) {
+                System.out.println("Skipping network scan.");
+            } else {
+                System.out.println("Unknown response.");
             }
         }
     }
+
+    private void runScan() {
+        try {
+            networks = Collections.list(NetworkInterface.getNetworkInterfaces());
+            System.out.format("There are %d networks detected.\n", networks.size());
+
+            for (NetworkInterface network : networks) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("Display Name: " + network.getDisplayName() + "\n");
+                sb.append("\t\tIP Addresses:\n");
+                networkAddresses.add(Collections.list(network.getInetAddresses()));
+                for (InetAddress address : networkAddresses.get(networkAddresses.size() - 1)) {
+                    sb.append("\t\t" + address.getHostAddress() + "\n");
+                }
+                System.out.println(sb);
+            }
+            
+        } catch (SocketException e) {
+            System.err.println(e.getMessage());
+        }
+    }
     
-    public InetAddress getAddress() {
-        return address;
+    public ArrayList<ArrayList<InetAddress>> getAddresses() {
+        return networkAddresses;
+    }
+
+    public String getNetworkDisplayName() {
+        return networkDisplayName;
     }
 
 
