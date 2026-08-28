1524ca47c1f55bfe3abfa6d70f1ee67ed80ea4c5
Banman03 <mckeownbaenan123@gmail.com>
2026-08-28 12:29:28 -0500

    client is able to send and receive data

--- app/src/main/java/java_http/client/Client.java
+++ app/src/main/java/java_http/client/Client.java
@@ -1,26 +1,77 @@
 package java_http.client;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.util.Scanner;
 
 public class Client {
-    Socket socket;
-    Integer port;
-    SocketAddress address;
+    private Socket socket;
+    private Integer port;
+    private InetAddress address;
+    private OutputStream socketWriteBuffer;
+    private InputStream socketReadBuffer;
+    private final static int RECEIVE_BUFFER_SIZE = 8192;
 
     public Client() {
         System.out.println("What port should the client be bound to?\n");
         Scanner sc = new Scanner(System.in);
         port = sc.nextInt();
 
+        address = InetAddress.getLoopbackAddress();
         try {
-            socket = new Socket(InetAddress.getLoopbackAddress(), port);
+            socket = new Socket(address, port);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        socket.bind(address);
+    }
+
+    public void writeData(String data) {
+        try {
+            if (socketWriteBuffer == null) {
+                socketWriteBuffer = socket.getOutputStream();
+            }
+            
+            if (!socket.isOutputShutdown()) {
+                    socket.setSendBufferSize(data.getBytes().length);
+                    socketWriteBuffer.flush();
+                    socketWriteBuffer.write(data.getBytes());
+                    System.out.println("Data sent.\n");
+            } else {
+                System.out.println("Write stream has already been shut down.\n");
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+
+    }
+
+    public byte[] readData() {
+        int totalBytesRead = 0;
+        try {
+            if (socketReadBuffer == null) {
+                socketReadBuffer = socket.getInputStream();
+            }
+            
+            if (!socket.isInputShutdown()) {
+                socket.setReceiveBufferSize(RECEIVE_BUFFER_SIZE);
+                System.out.format("Estimated reading %d bytes.\n", socketReadBuffer.available());
+                byte[] input = new byte[RECEIVE_BUFFER_SIZE];
+                totalBytesRead = socketReadBuffer.read(input);
+                System.out.format("Read %d bytes.\n", totalBytesRead);
+                return input;
+            } else {
+                System.out.println("Read stream has already been shut down.\n");
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        if (totalBytesRead == 0) {
+            System.out.println("0 bytes were read. Error somewhere?\n");
+        }
+        return new byte[0];
     }
 }
