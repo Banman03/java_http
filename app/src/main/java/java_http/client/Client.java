7d8cd808339ce3dadd3a62e28631799275109a19
Banman03 <mckeownbaenan123@gmail.com>
2026-08-28 11:39:11 -0500

    server should be able to listen and accept connections

--- app/src/main/java/java_http/client/Client.java
+++ app/src/main/java/java_http/client/Client.java
@@ -1,5 +1,26 @@
 package java_http.client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Scanner;
+
 public class Client {
-    
+    Socket socket;
+    Integer port;
+    SocketAddress address;
+
+    public Client() {
+        System.out.println("What port should the client be bound to?\n");
+        Scanner sc = new Scanner(System.in);
+        port = sc.nextInt();
+
+        try {
+            socket = new Socket(InetAddress.getLoopbackAddress(), port);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        socket.bind(address);
+    }
 }
--- app/src/main/java/java_http/server/Server.java
+++ app/src/main/java/java_http/server/Server.java
@@ -3,51 +3,70 @@ package java_http.server;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.UnknownHostException;
-import java.net.InetAddress;
 import java.util.Scanner;
+import java.net.Socket;
 
 public class Server {
     private ServerSocket socket;
     private InetSocketAddress address;
     private Integer port;
+    private Socket clientSocket;
+
     
     public Server() {
         System.out.println("What port should the server be bound to?\n");
         Scanner sc = new Scanner(System.in);
         port = sc.nextInt();
-        sc.close();
 
         try {
-            socket = new ServerSocket(port);
+            socket = new ServerSocket();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
 
-        try {
-            address = new InetSocketAddress(InetAddress.getByName(""), port);
-        } catch (UnknownHostException e) {
-            System.err.println(e.getMessage());
-        }
+        address = new InetSocketAddress(port);
     }
 
     public void bindLocally() {
-        try {
-            socket.bind(address);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+        if (socket != null && !socket.isBound()) {
+            try {
+                socket.bind(address);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+            System.out.format("Socket successfully bound to port %d and local address %s.\n", port, address.getAddress().getHostAddress());
+        } else {
+            System.out.format("Socket at port %d is unbound.\n", port);
         }
     }
 
     public void closeLocally() {
         try {
-            socket.close();
+            if (!socket.isClosed()) {
+                socket.close();
+                System.out.println("Socket successfully closed.\n");
+            } else {
+                System.out.format("Socket at port %d is already closed.\n", port);
+            }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    } 
+
+    public void acceptLocally() {
+        if (clientSocket == null) {
+            try {
+                clientSocket = socket.accept();
+                System.out.format("Conencted to the client socket at %s.\n", clientSocket.getInetAddress().getHostAddress());
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        } else {
+            System.out.format("The server has already accepted a connection to the socket at %s.\n", clientSocket.getInetAddress().getHostAddress());
+        }
     }
 
-    ServerSocket getServerSocket() {
+    public ServerSocket getServerSocket() {
         return socket;
     }
 }
